3b84ae347e406f1b00368865979acb3704ed3016
Darya Panina <33733507+smedashka@users.noreply.github.com>
2025-03-25 17:58:34 +0300

    Solve Exercise 04 - removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-};
+function removeFromArray(arr, ...args) {
+    for (let arg of args) {
+        arr = arr.filter((item) => item !== arg);
+    }
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
