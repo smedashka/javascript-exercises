5e716f33ec07053b1e87ec4510030a8c78eb1933
Darya Panina <33733507+smedashka@users.noreply.github.com>
2025-03-25 12:12:13 +0300

    Solve Exercise 03 - Reverse a String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    let res = '';
+    for (let i = 0; i < s.length; i++) {
+        res = s[i] + res;
+    }
+    return res;
 };
 
 // Do not edit below this line
