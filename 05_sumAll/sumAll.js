65a548832a60252efeb3263ed555e18da5362eea
Darya Panina <33733507+smedashka@users.noreply.github.com>
2025-03-26 11:45:13 +0300

    Solve Exercise 05 - sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  if (
+    x < 0 ||
+    y < 0 ||
+    !Number.isInteger(x) ||
+    !Number.isInteger(y)
+  ) {
+    return "ERROR";
+  }
 
+  if (x > y) {
+    let tmp = x;
+    x = y;
+    y = tmp;
+  }
+
+  let sum = 0;
+  for (let i = x; i <= y; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
