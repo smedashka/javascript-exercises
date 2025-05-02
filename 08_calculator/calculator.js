ebba6dbd57e0752fc45f82ab8ea9442e00c894dd
Darya Panina <33733507+smedashka@users.noreply.github.com>
2025-05-02 20:45:27 +0300

    Solve Exercise 08 - Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, currentItem) => sum + currentItem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, currentItem) => product * currentItem, 1) 
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let total = 1;
+	while (x != 0) {
+    total *= x;
+    x--;
+  }
+  return total;
 };
+
+console.log(factorial(5));
 
 // Do not edit below this line
 module.exports = {
