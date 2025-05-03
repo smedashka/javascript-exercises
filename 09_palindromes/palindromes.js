df143986b1735b9ce70135ea452c6ffa9010f3ee
Darya Panina <33733507+smedashka@users.noreply.github.com>
2025-05-03 22:57:57 +0300

    Solve Exercise 09 - Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let res = str
+    .toLowerCase()
+    .split("")
+    .filter(
+      (symbol) =>
+        (symbol.charCodeAt(0) >= 97 && symbol.charCodeAt(0) <= 122) ||
+        (symbol.charCodeAt(0) >= 48 && symbol.charCodeAt(0) <= 57)
+    );
+  return res.join("") === res.reverse().join("");
 };
 
 // Do not edit below this line
