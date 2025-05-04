9e0b29796f2f85392911d86fbb052d67f13df3cd
Darya Panina <33733507+smedashka@users.noreply.github.com>
2025-05-04 21:57:06 +0300

    Solve Exercise 10 - Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else if (n < 0) {
+    return "OOPS";
+  }
 
+  let f_i1 = 1;
+  let f_i2 = 0;
+  for (let i = 2; i <= n; i++) {
+    let f_i = f_i1 + f_i2;
+    f_i2 = f_i1;
+    f_i1 = f_i;
+  }
+  return f_i1;
 };
 
 // Do not edit below this line
