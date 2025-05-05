bfcef85ff1a7114bc468da057325e6e2d2dae460
Darya Panina <33733507+smedashka@users.noreply.github.com>
2025-05-05 19:19:52 +0300

    Solve Exercise 12 - Find the Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+function getAge(person) {
+  if (person.yearOfDeath === undefined) {
+    person.yearOfDeath = new Date().getFullYear();
+  }
+  return person.yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function (people) {
+  let peopleOldestToYoungest = people.toSorted((a, b) => getAge(b) - getAge(a));
+  return peopleOldestToYoungest[0];
 };
+
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
 
 // Do not edit below this line
 module.exports = findTheOldest;
